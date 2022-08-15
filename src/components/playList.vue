8401d75c79ef143dd562f8f0c1653287b496d1f4
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-08-14 21:15:53 -0600

    finished

--- src/components/playList.vue
+++ src/components/playList.vue
@@ -27,13 +27,16 @@ export default {
     // display song will display song by the value it got from sent song from songlist component
     display_song(sent_song) {
       // if conditional will check if there is any previous value of sent song is there inside the playlist ref
+      // if there is h2 element present before playlist ref it will get deleted
       // and if there is a value present it will prevent the repetion of adding a song to playlist
       if (this.$refs[`playlist`][`innerHTML`].includes(sent_song) === false &&  this.$refs[`playlist`][`previousElementSibling`][`localName`] === `h2`) {
         this.$refs[`playlist`][`previousElementSibling`][`outerHTML`] = "";
         this.$refs[`playlist`][`innerHTML`] += sent_song;
+        // now the previous h2 element got deleted and now the statment wil add other songs one by one
       } else if(this.$refs[`playlist`][`innerHTML`].includes(sent_song) === false && this.$refs[`playlist`][`previousElementSibling`][`localName`] !== `h2`){
         this.$refs[`playlist`][`innerHTML`] += sent_song;
       }
+      // if one is clicked more than once there will be an error
        else if (this.$refs[`playlist`][`innerHTML`].includes(sent_song)=== true )  {
         alert(`Cannot add one song more than once`);
 
